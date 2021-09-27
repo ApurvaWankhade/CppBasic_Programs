1075c42c650ee716566913fa3cad0f74fe22c174
ApurvaWankhade <82770486+ApurvaWankhade@users.noreply.github.com>
2021-09-27 23:17:31 +0530

    Add files via upload

---
+++ Insertion sort.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n,key,j;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++){
+	  cin>>arr[i];		
+	}
+	
+	for(int i=1;i<n;i++){
+	  key=arr[i];
+	   j=i;
+	  while(j>=0 && arr[j-1]>key){
+	  	arr[j]=arr[j-1];
+	  	j--;	  	
+	  }
+	  arr[j]=key;
+	}
+   for(int i=0;i<n;i++){
+	  cout<<arr[i]<<" ";
+   }
+	return 0;
+}
